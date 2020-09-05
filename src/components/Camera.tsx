4f246c89d8eefae72a4a5596152f32d557d3cd59
Alisamar Husain <zrthxn@gmail.com>
2020-09-05 23:25:32 +0530

    Working cam

--- src/components/Camera.tsx
+++ src/components/Camera.tsx
@@ -1,7 +1,7 @@
 import React, { useState, useRef } from 'react'
 
 export default function Camera() {
-  const videoRef = useRef<HTMLVideoElement>(new HTMLVideoElement())
+  const videoRef = useRef<HTMLVideoElement>(document.createElement('video'))
   const [isStreamOpen, setStreamOpen] = useState(false)
   // const [mediaStream, setMediaStream] = useState(undefined)
   
@@ -11,11 +11,15 @@ export default function Camera() {
   navigator.mediaDevices.getUserMedia({
     audio: true,
     video: {
-      facingMode: 'user'
+      facingMode: 'user',
+      width: { min: 1024, ideal: 1280, max: 1920 },
+      height: { min: 576, ideal: 720, max: 1080 }
     }
   }).then( stream => {
     setStreamOpen(true)
     videoRef.current.srcObject = stream;
+  }).catch( err =>{
+    console.error(err)
   })
   
   return (
@@ -23,7 +27,7 @@ export default function Camera() {
       {
         isStreamOpen ? (
           <video ref={videoRef} onCanPlay={videoRef.current.play} 
-            width="500px" height="500px" autoPlay playsInline 
+            width="-500px" height="auto" autoPlay playsInline 
           />
         ) : (
           <p>No video</p>
